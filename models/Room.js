fd91e10ca00c1ec0d5a72d535acdcc36452a293f
lsy8658 <socszz@gmail.com>
2023-08-26 17:19:10 +0900

    Room router 작성

--- models/Room.js
+++ models/Room.js
@@ -4,23 +4,23 @@ const { Schema } = mongoose;
 
 const RoomSchema = new Schema(
   {
-    // title: {
-    //   type: String,
-    //   require: true,
-    // },
-    // price: {
-    //   type: Number,
-    //   require: true,
-    // },
-    // maxPeople: {
-    //   type: Number,
-    //   require: true,
-    // },
-    // desc: {
-    //   type: String,
-    //   require: true,
-    // },
-    // roomNumbers: [{ number: Number, unavailableDates: [{ type: Date }] }],
+    title: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    maxPeople: {
+      type: Number,
+      require: true,
+    },
+    desc: {
+      type: String,
+      require: true,
+    },
+    roomNumbers: [{ number: Number, unavailableDates: [{ type: Date }] }],
   },
   { timestamps: true }
 );
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,15 +1,21 @@
 import express from "express";
-
+import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  updateRoom,
+} from "../controllers/room.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("rooms");
-});
 //CREATE
-
+router.post("/:hotelid", verifyAdmin, createRoom);
 //UPDATE
-
+router.put("/:id", verifyAdmin, updateRoom);
 //DELETE
-
+router.delete("/:id", verifyAdmin, deleteRoom);
 //GET
+router.get("/:id", getRoom);
+//GET ALL
+router.get("/", getRoom);
 export default router;
